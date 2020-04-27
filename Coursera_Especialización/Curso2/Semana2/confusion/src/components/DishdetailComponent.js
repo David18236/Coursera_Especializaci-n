730bd2610070b553232f3702c2380c5a60953847
David Ospina <dospinao@unal.edu.co>
2020-04-27 16:23:18 -0500

    Module SPA Part2 Completed

--- Coursera_Especialización/Curso2/Semana2/confusion/src/components/DishdetailComponent.js
+++ Coursera_Especialización/Curso2/Semana2/confusion/src/components/DishdetailComponent.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
 
 function RenderComments({ comments }) {
-
     if (comments != null) {
-        let comentarioschaval = comments.map((comm) => {
-            return (
-                <li key={comm.id}>
-                    <p> {comm.comment} </p>
-                    <p> -- {comm.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comm.date)))} </p>
-                </li>
-            );
-        });
-
         return (
-            <div className="col-12 col-sm-5">
-                <h4> <strong> Comments </strong> </h4>
-
+            <div className="col-md-12 col-sm-12 col-xm-12 m-1">
+                <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    {comentarioschaval}
+                    {comments.map((comment) => {
+                        return (
+                            <li key={comment.id}>
+                                <span className="text">{comment.comment} <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p></span>
+                            </li>
+                        );
+                    })}
                 </ul>
             </div>
         );
-
     }
 
     else {
@@ -32,16 +28,17 @@ function RenderComments({ comments }) {
 
 function RenderDish({ dish }) {
     return (
+        <div className="row">
+            <div className="col-12 m-1">
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
 
-        <div className="col-12 col-sm-5">
-            <Card>
-                <CardImg width="100%" src={dish.image} alt={dish.name} />
-
-                <CardBody>
-                    <CardTitle> {dish.name} </CardTitle>
-                    <CardText> {dish.description} </CardText>
-                </CardBody>
-            </Card>
+                    <CardBody>
+                        <CardTitle> {dish.name} </CardTitle>
+                        <CardText> {dish.description} </CardText>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     );
 }
@@ -51,10 +48,27 @@ const DishDetail = (props) => {
 
     if (dish != null) {
         return (
-            <div class="container">
+            <div className="container">
                 <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
+                    </div>
                 </div>
             </div>
         );
